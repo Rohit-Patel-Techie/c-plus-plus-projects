ce01ce7141b00df40c38cf890386c8f7211580e6
Rohit Patel <rp380972@gmail.com>
2026-03-15 10:13:17 +0530

    Done main.py

--- DU Smart SGPA Calculator/main.cpp
+++ DU Smart SGPA Calculator/main.cpp
@@ -1,39 +1,88 @@
 #include<iostream>
 #include<vector>
 #include<iomanip> 
+
+#include "Student.h"
 #include "calculator.h"
 #include "GradeUtils.h"
 using namespace std;
 
-void display();
-int input_course_detail();
-double sgpaCalculator(int total_num_course);
+void Headerdisplay();
+int input_total_course();
+void inputSubjects(vector<Subject> subjects, int totalCourse);
+void sgpaCalculator(vector<Subject> subjects);
 
 int main(){
 
-    display();
+    Headerdisplay();
 
-    int total_course = input_course_detail();
+    int total_course = input_total_course(); //Total courses enrolled student
+    if(total_course <= 0){
+        cout<<"ERROR : Invalid Number of Courses. Existing the program.\n";
+        return 0;
+    }
 
-    double final_sgpa = sgpaCalculator(total_course);
+    vector<Subject> subjects;
+    inputSubjects(subjects, total_course);
+    
+    
+    sgpaCalculator(subjects); // Final SGPA Calculation
+    
 
     return 0;
 }
 
-void display(){
+void Headerdisplay(){
     cout<<"\n***********************************************************************\n";
     cout<<"\tWelcome To DU Academic Performance Calculator(SGPA/CGPA)";
     cout<<"\n***********************************************************************\n";
 }
 
-int  input_course_detail(){
+int  input_total_course(){
     int num_course;
-    cout<<"Enter the Number of Course Enrolled : ";
+    cout<<"Enter the Number of Subject Enrolled : ";
     cin>>num_course;
     return num_course;
 }
 
+void inputSubjects(vector<Subject> subjects, int totalCourse){
+    for(int i = 0; i < totalCourse; i++){
+        Subject temp;
 
-double sgpaCalculator(int total_num_course){
+        cout<<"\n-------------------Course "<< i + 1 <<" ----------------\n";
 
+        cout<<"Enter Course Name : ";
+        cin>>temp.name;
+
+        cout<<"Enter Course Credits : ";
+        cin>>temp.credit;
+
+        if(temp.credit <= 0){
+            cout<<"Invalid Credits! Try Again.\n";
+            i--;
+            continue;
+        }
+
+        cout<<"Enter Grade (O, A+, A, B+, B, C, D, F) : ";
+        cin>>temp.grade;
+        if(getGradePoint(temp.grade) == -1){
+            cout<<"Invalid Grade! Please Enter again.\n";
+            i--;
+            continue;
+        }
+        subjects.push_back(temp);
+    }
+}
+
+
+void sgpaCalculator(vector<Subject> subjects){
+    double sgpa = calculateSGPA(subjects);
+
+    cout << fixed << setprecision(2);
+
+    cout << "\n====================================";
+    cout << "\nYour Calculated SGPA is : " << sgpa;
+    cout << "\n====================================\n";
+
+    cout << "\nThank you for using DU Academic Performance Calculator\n";
 }
